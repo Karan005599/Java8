55f3f0549183001573edc0f97bba11ae6a760a98
Suraj <surajkumarmohapatra111@gmail.com>
2024-07-09 02:08:13 +0530

    This is the new Project

---
+++ src/Lamda.java
@@ -0,0 +1,6 @@
+public class Lamda {
+    public static void main(String[] args) {
+
+        // lamda expression is an anonymous function which don't have any name , modifier   and return type
+    }
+}
