ef34abdd99336afc46c0436a525661e50f04ba0c
Suraj <surajkumarmohapatra111@gmail.com>
2024-07-11 03:20:47 +0530

    Lambda Expression Implemntation

--- src/com/Java8_Practice/Lambda.java
+++ src/com/Java8_Practice/Lambda.java
@@ -1,6 +1,7 @@
 package com.Java8_Practice;
 // lambda expression is an anonymous function which don't have any name , modifier   and return type amd we can use it with the help of functional interface
 //  : the interface which having only one abstract method by have one or more default or static method
+// Using lambda expression we don't need to use separate implementation
 public class Lambda {
     public static void main(String[] args) {
 
