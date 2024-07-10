0312e361d84f77db7b030a833f3237f61b33b084
Suraj <surajkumarmohapatra111@gmail.com>
2024-07-11 03:14:16 +0530

    Lambda Expression Implemntation

--- src/com/Java8_Practice/Employee.java
+++ src/com/Java8_Practice/Employee.java
@@ -1,5 +1,6 @@
 package com.Java8_Practice;
 // Functional Interface for Lambda Expression
+// Functional Interface reference can be used to hold lambda Expression
 public interface Employee {
     String getName();
 }
